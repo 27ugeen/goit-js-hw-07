bd72bb63dfcb87f995a459445c47a072222dcaf7
Yevhen Havrysh <39010905+27ugeen@users.noreply.github.com>
2024-06-17 16:01:02 +0300

    removed comments

--- js/task-4.js
+++ js/task-4.js
@@ -4,29 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.querySelector('.login-form');
 
   loginForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
 
-    // Access form elements
     const emailInput = this.elements.email;
     const passwordInput = this.elements.password;
 
-    // Trim and get values
     const emailValue = emailInput.value.trim();
     const passwordValue = passwordInput.value.trim();
 
-    // Check if fields are empty
     if (emailValue === '' || passwordValue === '') {
       alert('All form fields must be filled in');
       return;
     }
 
-    // Create object with form data
     const formData = {
       email: emailValue,
       password: passwordValue,
     };
 
-    // Log form data and reset form
     console.log(formData);
     loginForm.reset();
   });
--- js/task-5.js
+++ js/task-5.js
@@ -6,19 +6,14 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-// Select elements
 const changeColorButton = document.querySelector('button.change-color');
 const colorSpan = document.querySelector('span.color');
 const body = document.body;
 
-// Add click event listener to the button
 changeColorButton.addEventListener('click', function () {
-  // Generate random color
   const randomColor = getRandomHexColor();
 
-  // Change background color of body
   body.style.backgroundColor = randomColor;
 
-  // Update span text with the color
   colorSpan.textContent = randomColor;
 });
